511f6d253db82b4044aa6f654ecb419d638a219a
Cade Gillem <cade.p.gillem@wmich.edu>
2017-07-06 16:57:16 -0400

    Implement admin panel with link to Tours.

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+  <a href="{{ url('admin/tours') }}">Tours</a>
+@endsection
---
+++ tests/Browser/AdminTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use App\User;
+
+class AdminTest extends DuskTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testMainPanel()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->user)
+                ->visit('/admin')
+                ->assertSeeLink('Tours')
+                ->clickLink('Tours')
+                ->assertPathIs('/campustours2/admin/tours');
+        });
+    }
+}
