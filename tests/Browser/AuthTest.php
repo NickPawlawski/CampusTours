b6bdacadac669e308a0bf728fc02455eb9443815
Cade Gillem <cade.p.gillem@wmich.edu>
2017-07-06 16:56:10 -0400

    Implement logging in for users.

---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use App\User;
+
+class AuthTest extends DuskTestCase
+{
+    //use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', $this->user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/campustours2/home');
+        });
+    }
+}
