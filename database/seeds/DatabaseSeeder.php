470481c4d7e9a1069afd784ea7edf6fe853ca545
Cade Gillem <cade.p.gillem@wmich.edu>
2017-07-13 13:46:28 -0400

    Change username/email for seed user.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 1)->create();
+        factory(App\User::class, 1)->create([
+            'email' => 'test@test.com',
+            'username' => 'test'
+        ]);
     }
 }
