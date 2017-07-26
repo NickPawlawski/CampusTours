5a033e2c77bfab9435a0e04cc641abf796845671
Cade Gillem <cade.p.gillem@wmich.edu>
2017-07-26 12:32:46 -0400

    Rename browser test to stay consistent.

--- tests/Browser/AdminToursTest.php
+++ tests/Browser/AdminToursTest.php
@@ -20,24 +20,31 @@ class AdminTourTest extends DuskTestCase
 
     public function testToursPanel()
     {
+        Tour::truncate();
+
         $this->tour = factory(Tour::class)->create();
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs($this->user)
                     ->visit('/admin/tours')
                     ->assertSee('Tour Time')
-                    ->assertSee($this->tour->tourtime->toDateTimeString()); // Assert that a table row exists for the tour
+                    // Assert that a table row exists for the tour
+                    ->assertSee(date('m/d/Y h:m A', $this->tour->tourtime->timestamp)); 
         });
     }
 
     public function testAddTour()
     {
+        Tour::truncate();
+
         $this->browse(function (Browser $browser) {
             $browser->loginAs($this->user)
                     ->visit('/admin/tours')
                     ->value('#addDate', '01/01/2020')
                     ->value('#addTime', '12:00 PM')
-                    ->press('Add Tour')
+                    ->press('Add Tour');
+
+            $browser->loginAs($this->user)
                     ->visit('/admin/tours')
                     ->assertSee('01/01/2020')
                     ->assertSee('12:00 PM');
