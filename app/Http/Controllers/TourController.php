362fee6f3b5388c30a97406ef8c81df05e5e9d1a
Cade Gillem <cade.p.gillem@wmich.edu>
2017-07-27 14:25:04 -0400

    Add documentation to TourController.

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -8,40 +8,49 @@ use Carbon\Carbon;
 
 class TourController extends Controller
 {
+    /* Shows a view containing a list of tours, along with
+       forms for adding and filtering them. */
     public function index(Request $request) {
         $toursQuery = Tour::query();
 
+        // If the tours are to be filtered by date, add this to the query.
         if ($request->has('filterDate')) {
             $toursQuery = $toursQuery->where('date', '=', $request->get('filterDate'));
         }
 
+        // Finish the query by sorting and paginating.
         $tours = $toursQuery
             ->orderBy('time', 'desc')
             ->orderBy('date', 'desc')
             ->paginate(15);
 
+        // Return the tours view.
         return view('admin.tours', [
             'tours' => $tours,
         ]);
     }
 
+    /* Takes information about a new tour and creates it in the database. */
     public function create(Request $request){
-        // Validate
+        // Validate the new date and time.
         $this->validate($request, [
                 'addDate' => 'required|date_format:Y-m-d',
                 'addTime' => 'required|date_format:H:i'
             ]);
 
-        // Create the new tour
+        // Create the new tour.
         $tour = new Tour();
+        // The date will be in the form of a Carbon object.
         $tour->date = new Carbon($request->get('addDate'));
+        // The time will be a properly formatted string.
         $tour->time = date('H:i:s', strtotime($request->get('addTime')));
         $tour->save();
 
-        // Redirect to the main tours panel
+        // Redirect to the main tours panel.
         return redirect()->action('TourController@index');
     }
 
+    /* Displays information about a tour. */
     public function show(Request $request, $id)
     {
         $tour = Tour::find($id);
@@ -51,6 +60,7 @@ class TourController extends Controller
         ]);   
     }
 
+    /* Deletes a tour. */
     public function delete(Request $request)
     {
         Tour::find($request->id)->delete();
@@ -58,6 +68,7 @@ class TourController extends Controller
         return redirect()->action('TourController@index');
     }
 
+    /* Displays deleted tours and allows them to be restored. */
     public function deleted(Request $request)
     {
         $tours = Tour::onlyTrashed()->get();
@@ -67,6 +78,7 @@ class TourController extends Controller
             ]);
     }
 
+    /* Restores a tour from deletion. */
     public function restore(Request $request, $id)
     {
         $tour = Tour::withTrashed()->find($id);
