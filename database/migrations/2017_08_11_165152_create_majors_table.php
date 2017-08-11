69e69b6ed731edf6b8823e3605c0711999af8b4e
NickPawlawski <nicholas.a.pawlawski@wmich.edu>
2017-08-11 13:15:59 -0400

    Create migrations for major and attendees tables

---
+++ database/migrations/2017_08_11_165152_create_majors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMajorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('majors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('code');
+            $table->boolean('undergraduate');
+            $table->boolean('graduate');
+            $table->boolean('active');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('majors');
+    }
+}
---
+++ database/migrations/2017_08_11_165939_create_attendees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('phone');
+            $table->enum('studentType',['transfer','freshman','graduate','other']);
+            $table->text('considerations');
+            $table->integer('visitors');
+            
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendees');
+    }
+}
