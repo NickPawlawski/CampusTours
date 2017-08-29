13f9e955d9f2318cdd0d187d8326deaf0b1851f3
NickPawlawski <nicholas.a.pawlawski@wmich.edu>
2017-08-29 11:19:58 -0400

    create button on admin page for majors

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,6 +7,7 @@
 	<div class="row column">
 		<ul class="stack button-group">
 			<li><a class="button" href="{{ url('admin/tours') }}">Tours</a></li>
+			<li><a class="button" href="{{ url('admin/majors') }}">Majors</a></li>
 		</ul>
 	</div>
 @endsection
